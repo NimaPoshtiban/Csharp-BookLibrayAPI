6eb365d28305fa1b412a1899428e142106fe36cb
Nima Poshtiban <poshtibannima@gmail.com>
2022-04-25 22:34:08 +0430

    Generated needed dtos and methods for intercating with api using nswag and registered them in DI Container

--- BookLibraryUI/Program.cs
+++ BookLibraryUI/Program.cs
@@ -1,4 +1,5 @@
-using BookLibraryUI.Data;
+
+using BookLibraryUI.Services.Base;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -7,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+// consuming API
+builder.Services.AddHttpClient<IClient,Client>(cl=>cl.BaseAddress= new Uri("https://localhost:7105"));
 
 var app = builder.Build();
 
